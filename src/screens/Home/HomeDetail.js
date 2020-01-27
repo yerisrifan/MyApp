fb30394d83f3bc09b544220583fd3bac9428cb8a
Yeris <bahtiaryeris@gmail.com>
2020-01-28 01:35:17 +0700

    working on details

--- src/screens/Home/HomeDetail.js
+++ src/screens/Home/HomeDetail.js
@@ -5,9 +5,9 @@ import {
   Icon,
   TopNavigationAction,
   Text,
-  Button,
+  Layout,
 } from '@ui-kitten/components';
-import {SafeAreaView} from 'react-native';
+import {SafeAreaView, StyleSheet, Image} from 'react-native';
 
 const HomeDetail = props => {
   const BackIcon = style => <Icon {...style} name="arrow-ios-back-outline" />;
@@ -20,10 +20,32 @@ const HomeDetail = props => {
   return (
     <SafeAreaView style={styles.container}>
       <TopNavigation leftControl={BackAction()} title="Application Title" />
-      <Text>Details</Text>
-      <Button onPress={() => props.navigation.navigate('Player')}>Play</Button>
+      <Layout style={style.containerDetail}>
+        <Layout style={style.containerImage}>
+          <Image
+            style={style.poster}
+            source={{uri: 'https://via.placeholder.com/150/56a8c2'}}
+          />
+        </Layout>
+        <Text>single</Text>
+      </Layout>
     </SafeAreaView>
   );
 };
 
+const style = StyleSheet.create({
+  containerDetail: {
+    backgroundColor: 'red',
+  },
+  containerImage: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginVertical: 30,
+  },
+  poster: {
+    width: 300,
+    height: 300,
+    borderRadius: 200,
+  },
+});
 export default HomeDetail;
--- src/screens/Player/Player.js
+++ src/screens/Player/Player.js
@@ -8,6 +8,9 @@ import {
 } from '@ui-kitten/components';
 import {styles} from '../../constant';
 import {SafeAreaView, StyleSheet, Image} from 'react-native';
+import Control from './Control';
+import Slider from '@react-native-community/slider';
+import Video from 'react-native-video';
 
 export default class Player extends Component {
   render() {
@@ -22,20 +25,22 @@ export default class Player extends Component {
     );
     return (
       <SafeAreaView style={styles.container}>
-        <TopNavigation
-          rightControls={BackAction()}
-          alignment="center"
-          title="Player"
-        />
-        <Layout style={style.containerHeader}>
-          <Text>BlackSwan</Text>
-          <Text>BTS</Text>
-        </Layout>
-        <Layout style={style.containerImage}>
-          <Image
-            style={style.poster}
-            source={{uri: 'https://via.placeholder.com/600/92c952'}}
-          />
+        <TopNavigation rightControls={BackAction()} />
+        <Layout style={style.containerMain}>
+          <Layout style={style.containerHeader}>
+            <Text style={style.title}>BlackSwan</Text>
+            <Text style={style.artist}>BTS</Text>
+          </Layout>
+          <Layout style={style.containerImage}>
+            <Image
+              style={style.poster}
+              source={{uri: 'https://via.placeholder.com/600/92c952'}}
+            />
+          </Layout>
+          <Layout>
+            <Slider />
+            <Control />
+          </Layout>
         </Layout>
       </SafeAreaView>
     );
@@ -43,19 +48,35 @@ export default class Player extends Component {
 }
 
 const style = StyleSheet.create({
+  containerMain: {
+    marginTop: 30,
+    paddingHorizontal: 30,
+  },
   containerHeader: {
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'red',
+    marginBottom: 20,
   },
+  title: {
+    fontFamily: 'Montserrat-Bold',
+    fontSize: 20,
+    fontWeight: '500',
+  },
+  artist: {
+    marginTop: 10,
+    fontSize: 15,
+    fontWeight: '200',
+    fontFamily: 'Montserrat-Regular',
+  },
+
   containerImage: {
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'yellow',
+    marginVertical: 30,
   },
   poster: {
-    width: 350,
-    height: 350,
+    width: 300,
+    height: 300,
     borderRadius: 200,
   },
 });
